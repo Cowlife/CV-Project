3db85ec06b1329c08a3c41c33a98e10c940d4d46
Cowlife <56398689+Cowlife@users.noreply.github.com>
2022-12-28 18:57:24 +0000

    More changes
    Sun rotation.

--- cv/entities/Entity.gd
+++ cv/entities/Entity.gd
@@ -1,19 +1,16 @@
-extends DirectionalLight
+extends KinematicBody
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-onready var light_source = get_node(".")
-onready var timer = $Timer as Timer
 
 # Called when the node enters the scene tree for the first time.
-
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	light_source.rotate_x(0.001)
+	pass
