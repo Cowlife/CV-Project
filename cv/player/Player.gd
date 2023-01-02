4f6c9c7b653b514889d81eb8553ca0ab0defd73d
martimf3 <martimmrcf1@gmail.com>
2023-01-02 17:56:51 +0000

    Movement + Camera_Follow
    fdrgdf

--- cv/player/Player.gd
+++ cv/player/Player.gd
@@ -1,36 +1,57 @@
 extends KinematicBody
 
-export var max_speed = 12
-export var gravity = 70
-export var jump_impulse = 25
-
+var gravity = -9.8
 var velocity = Vector3.ZERO
+var camera
+var character 
+const SPEED = 8
+const ACCELERATION = 3
+const DE_ACCELERATION = 5
 
-onready var pivot = $Pivot
+func _ready():
+	character = get_node(".")
 
 func _physics_process(delta):
-	var input_vector = get_input_vector()
-	apply_movement(input_vector)
-	apply_gravity(delta)
-	velocity = move_and_slide(velocity, Vector3.UP)
+	var is_moving = false
+	camera = get_node("Target/Camera").get_global_transform()
+	var dir = Vector3.ZERO
 	
-func get_input_vector():
-	var input_vector = Vector3.ZERO
-	input_vector.x = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
-	input_vector.z = Input.get_action_strength("move_forward") - Input.get_action_strength("move_back")
+	if(Input.is_action_pressed("move_forward")):
+		dir += -camera.basis[2]
+		is_moving = true
+	if(Input.is_action_pressed("move_back")):
+		dir += camera.basis[2]
+		is_moving = true
+	if(Input.is_action_pressed("move_left")):
+		dir += -camera.basis[0]
+		is_moving = true
+	if(Input.is_action_pressed("move_right")):
+		dir += camera.basis[0]
+		is_moving = true
+		
+	dir.y = 0
+	dir = dir.normalized()
 	
-	return input_vector.normalized()
+	velocity.y += delta * gravity
+	var hv = velocity
+	hv.y = 0
+	var new_pos = dir * SPEED
+	var accel = DE_ACCELERATION
 	
-func apply_movement(input_vector):
-	velocity.x = input_vector.x * max_speed
-	velocity.z = input_vector.z * max_speed
+	if (dir.dot(hv) > 0):
+		accel = ACCELERATION
 	
-	if input_vector != Vector3.ZERO:
-		pivot.look_at(translation + input_vector, Vector3.UP)
+	hv = hv.linear_interpolate(new_pos, accel * delta)
+	velocity.x = hv.x
+	velocity.z = hv.z
 	
-func apply_gravity(delta):
-	velocity.y -= gravity * delta
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
 	
-func jump():
-	if is_on_floor() and Input.is_action_pressed("jump"):
-		velocity.y = jump_impulse
+	if is_moving:
+		var angle = atan2(hv.x, hv.z)
+		var char_rot = character.get_rotation()
+		char_rot.y = angle
+		character.set_rotation(char_rot)
+	
+	var speed = hv.length() / SPEED
+
