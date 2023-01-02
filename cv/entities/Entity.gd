1ec51b8924106bac66f2e43747e91360d8af4f14
Cowlife <56398689+Cowlife@users.noreply.github.com>
2023-01-02 16:15:32 +0000

    Entity Movement
    Movement

--- cv/entities/Entity.gd
+++ cv/entities/Entity.gd
@@ -2,8 +2,8 @@ extends KinematicBody
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var speed = 2
+var point = Vector3(52, 12, 0)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,4 +13,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var direction
+	
+	if point.distance_to(transform.origin) > 0:
+		direction = point - transform.origin
+		direction = direction.normalized() * speed
+	
+	else:
+		direction = point - transform.origin
+	
+	move_and_slide(direction)
